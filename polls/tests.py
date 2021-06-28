21355222aee7a11213700cc5e2fe2c1cec90fdce
platonicaaa <platonicaaa@hotmail.com>
2021-06-28 15:28:17 +0200

    Update details test

--- polls/tests.py
+++ polls/tests.py
@@ -102,21 +102,24 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1],
         )
 
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
-        Future question should not be retrievable => 404
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
         """
-        future_question = create_question('A question?', 30)
+        future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         """
-        Past question should be retried => 200 & displays the question's text.
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
         """
-        past_question = create_question('A question?', -1)
+        past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertContains(response, past_question.question_text)

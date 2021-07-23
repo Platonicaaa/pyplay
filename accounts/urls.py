69e2855fcef9ac4a0c7312608ce83a577c1b9c52
platon <platonicaaa@hotmail.com>
2021-07-23 16:30:31 +0200

    Update account urls. Apply missing migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from .views import SignupView
 
+app_name = 'accounts'
 urlpatterns = [
     path('', views.user_home, name='home'),
     path('signup/', SignupView.as_view(), name='signup')

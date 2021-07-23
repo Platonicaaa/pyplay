baf62931d9cec8519dd094c4f24e1e2ffbb2383f
platon <platonicaaa@hotmail.com>
2021-07-23 16:26:57 +0200

    Set user homes depending on user groups

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from . import views
 from .views import SignupView
 
 urlpatterns = [
+    path('', views.user_home, name='home'),
     path('signup/', SignupView.as_view(), name='signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
-from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from .forms import PyPlayyUserCreationForm
+from .utils import is_seller, is_buyer
+
+
+@login_required
+def user_home(request):
+    user = request.user
+    if is_buyer(user):
+        return HttpResponseRedirect(reverse('auctions:index'))
+    elif is_seller(user):
+        return HttpResponseRedirect(reverse('products:index'))
+    else:
+        return HttpResponseRedirect(reverse('auctions:index'))
 
 
 class SignupView(generic.CreateView):
